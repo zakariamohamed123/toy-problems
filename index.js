d01bebf2f5c33fcd9fc28868c2c96913e7c172b7
zakariamohamed123 <149509331+zakariamohamed123@users.noreply.github.com>
2023-10-31 22:25:22 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>STUDENT GRADE GENERATOR</title>
+</head>
+<body>
+    <h1>Students Grades</h1>
+
+<!--link to javascript where grades are calculated--> 
+    <script src="./net-salary-detector.js"></script>
+</body>
+</html>
